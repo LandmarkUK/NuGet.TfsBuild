0dfa8209201adb6bbf021508190c680c265dfecb
Joel Hammond-Turner <joel.hammond-turner@landmark.co.uk>
2013-07-30 14:48:28 +0100

    It works!

---
+++ src/NuGet.TfsBuild.NuGetPackage/Package/tools/uninstall.ps1
@@ -0,0 +1,11 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+Add-Type -AssemblyName "Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
+
+$msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
+$importToRemove = $msbuild.Xml.Imports | Where-Object { $_.Project.Endswith('NuGet.TfsBuild.targets') }
+
+$msbuild.Xml.RemoveChild($importToRemove) | out-null
+$project.Save()
+
+Write-Host "Uninstalled NuGet.TfsBuild from $project.Name ..."
